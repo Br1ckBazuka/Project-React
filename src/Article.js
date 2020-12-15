ee3f29502fd28dc1cb427aa6baa310ec63e7efd4
Br1ckBazuka <br1ckbazuka2307@gmail.com>
2020-12-16 01:25:23 +0300

    ..

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TodoList from './components/Todo/Todolist';
+import {NavLink}  from 'react-router-dom';
 import TabbarSandbox from './components/tabbar/Sandbox';
 import './index.css';
 
@@ -10,6 +11,17 @@ class Article extends Component {
             <div>
                    <div className="Article_main_text">
                     <TodoList/>
+                    <ul className="articles_link">
+                           <li>
+                              <NavLink to="article1">Статья #1</NavLink>
+                           </li>
+                           <li>
+                              <NavLink to="article2">Статья #2</NavLink>
+                           </li>
+                           <li>
+                              <NavLink to="article3">Статья #3</NavLink>
+                           </li>
+                        </ul>
                     </div>,
 
                     <div className="Content_article">
